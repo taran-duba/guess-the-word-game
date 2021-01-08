d388bfe1f8be78900434e6801566417678064857
taran-duba <72531052+taran-duba@users.noreply.github.com>
2021-01-07 20:18:27 -0500

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,11 @@
+var score = 0;
+function updateScore() {
+    score = score + 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+function saveScore() {
+    localStorage.setItem("score", score);
+}
+function nextPage() {
+    window.location = "index.html";
+}
